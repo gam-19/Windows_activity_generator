94d8c3429f404bd019a3f6312e3a40d866caa4b6
gam-19 <122411234+gam-19@users.noreply.github.com>
2023-10-27 17:03:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import webbrowser
 countries = ["Argentina", "Bolivia", "Brasil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Perú", "Surinam", "Uruguay", "Venezuela"]
 
 def install_modules():
-    #Instalar módulo pyautogui si no está instalado
+    # Instalar módulo pyautogui si no está instalado
     try:
         import pyautogui
     except ImportError:
@@ -36,61 +36,40 @@ def main():
     # Convertir la duración de actividad a segundos
     activity_duration = now + activity_duration * 60
 
-    # Mientras el valor de hora actual sea menor al tiempo de duración de la actividad, realizar bucle de actividad
     while now < activity_duration:
-        # Frecuencia de actividad
         time.sleep(frequency)
 
-        # Escoger acción aleatoria
         action = random.choice([
             "Mover mouse",
             "Escribir en notepad",
             "Busqueda en chrome"
         ])
 
-        # Realizar acción
         if action == "Mover mouse":
+            import pyautogui  # Importa pyautogui solo cuando se necesita
             pyautogui.moveRel(random.randint(-100, 100), random.randint(-100, 100))
         elif action == "Escribir en notepad":
-            # Abrir la aplicación Notepad
+            import subprocess  # Importa subprocess solo cuando se necesita
+            import pyautogui  # Importa pyautogui solo cuando se necesita
             subprocess.Popen(["notepad.exe"])
             time.sleep(2)
             try:
-                # Escribir texto aleatorio
                 text = random.choice(["perruna", "lobezna", "me gusta el chori", "soy taca"])
                 pyautogui.typewrite(text)
                 time.sleep(2)
-
-                # Cerrar la aplicación Notepad
                 pyautogui.hotkey('alt', 'f4')
-
-                # Esperar a que aparezca el cuadro de diálogo
                 time.sleep(1)
-
-                # Simular presionar la tecla "N" para no guardar los cambios
                 pyautogui.press("n")
-
             except Exception as e:
                 print(f"An error occurred: {e}")
-
         elif action == "Busqueda en chrome":
-            # Abrir una búsqueda en Google
+            import pyautogui  # Importa pyautogui solo cuando se necesita
             webbrowser.open("https://www.google.com/search?q=capital+de+%s" % random.choice(countries))
             time.sleep(2)
-
-            # Cerrar la aplicación Chrome
             pyautogui.hotkey('alt', 'f4')
-
-            # Esperar a que aparezca el cuadro de diálogo de guardar
             time.sleep(1)
-
-            # Simular presionar la tecla "N" para no guardar los cambios
             pyautogui.press("n")
 
-    # Repetir proceso
-    main()
-
 if __name__ == "__main__":
-    # Instalar módulos si no están instalados
     install_modules()
     main()
